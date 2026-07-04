f557dd5f33d11066cb2740e0ccdc55a6386de77d
Priyanshiba27 <priyanshibagohil77@gmail.com>
2026-07-04 22:16:52 +0530

    commit

---
+++ assignment/module 2/TASK/main (3).c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n Priyanshiba Gohil");
+    printf("\n Rajkot");
+    printf("\n 27/11/05");
+    printf("\n bsc.physics");
+
+    getch();
+    
+}
